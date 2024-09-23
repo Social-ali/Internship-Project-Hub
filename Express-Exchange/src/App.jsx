1dfeca4c8789ac201f8bd7cb662000b3f730f3a3
social-ali <jafri6456@gmail.com>
2024-09-23 21:14:09 +0500

    Express-Exchange React project-3

---
+++ Express-Exchange/src/App.jsx
@@ -0,0 +1,12 @@
+import ConverterForm from "./components/ConverterForm"
+
+const App = () => {
+  return (
+    <div className="currency-converter">
+      <h2 className="converter-title">Exchange Express</h2>
+      <ConverterForm />
+    </div>
+  )
+}
+
+export default App;
